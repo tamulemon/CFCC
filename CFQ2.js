af9b17b02c0b4f0686ea3659ab1fd8582c623fce
Meng Chen <tamulemon@gmail.com>
2015-04-03 13:12:44 -0700

    small changes

--- CFQ2.js
+++ CFQ2.js
@@ -33,120 +33,43 @@ console.log('server is starting.');
 
 
 
-//// rewrite 03/29/2015
-//var http = require('http');
-//var fs = require('fs');
-//var url = require('url');
-//var server = http.createServer();
-//
-//server.on('request', function(request,response) {
-//    var rpathname = url.parse(request.url).path;
-//    console.log("Request for " + rpathname + " received.");
-//    if (rpathname === '/') {
-//        console.log('There comes a correct request...');
-//        response.writeHead(200, {'content-type':'text/html'});
-//        var file = fs.createReadStream('CFIndex.html');
-//        var errorHandler = function() {
-//            response.writeHead(500, {'content-type':'text/plain'}); 
-//            response.write(error);
-//        }; 
-//        file
-//            .on('error', errorHandler)
-//            .pipe(response, {end:false})
-//            .on('error', errorHandler);
-//        file.on ('close', function () {
-//            response.end(console.log('server is closing'));
-//        });
-//    } else {
-//        response.writeHead(404, {'content-type':'text/plain'});
-//        response.write('Incorrect url');
-//        response.end();
-//    }
-//});
-//
-//server.listen(8080, '127.0.0.1');
-//console.log('server is starting.');
-//
-//
-//
-//// rewrite 04/01/2015
-//var http = require('http');
-//var fs = require('fs');
-//var url = require('url');
-//var server = http.createServer();
-//var file = fs.createReadStream('CFIndex.html');
-//
-//server.on('request', function(request,response) {
-//    var rpathname = url.parse(request.url).path;
-//    console.log("Request for " + rpathname + " received.");
-//    if (rpathname === '/') {
-//        console.log('There comes a correct request...');
-//        response.writeHead(200, {'content-type':'text/html'});
-//        var errorHandler = function(error, content) {
-//            response.writeHead(500, {'content-type':'text/plain'}); 
-//            response.write(error);
-//        }; 
-//        file
-//            .on('error', errorHandler)
-//            .pipe(response, {end:false})
-//            .on('error', errorHandler);
-//        file.on ('close', function () {
-//            response.end(console.log('server is closing'));
-//        });
-//    } else {
-//        response.writeHead(404, {'content-type':'text/plain'});
-//        response.write('Incorrect url');
-//        response.end();
-//    }
-//});
-//
-//server.on('error', function(error, content) {
-//    console.log('on error:' + error);
-//});
-//
-//server.listen(8080, '127.0.0.1');
-//console.log('server is starting.');
+
+// rewrite 04/02/2015
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var server = http.createServer();
+
+var errorHandler = function(error, content) {
+    console.error('on error: ' + error);
+        }; 
+
+server.on('error', errorHandler);
+
+server.on('request', function(request,response) {
+    var rpathname = url.parse(request.url).path;
+    var file = fs.createReadStream('CFIndex.html');
+    file.on('error', errorHandler);
+//    file.emit('error', 'again!', 'abc');
+    console.log("Request for " + rpathname + " received.");
+    if (rpathname === '/') {
+        console.log('There comes a correct request...');
+        response.writeHead(200, {'content-type':'text/html'});
+        file
+            .pipe(response, {end:false})
+            .on('error', errorHandler);
+        file.on ('end', function () {
+            response.end(console.log('server is closing'));
+        });
+    } else {
+        response.writeHead(404, {'content-type':'text/plain'});
+        response.write('Incorrect url');
+        response.end();
+    }
+});
+
+//test code
+server.listen(8080, '127.0.0.1');
+console.log('server is starting.');
 //server.emit('error', 'Ohno', 'xyz');
-//file.emit('error', 'again!', 'abc');
-//
-//
-//
-//
-//
-//// rewrite 04/02/2015
-//var http = require('http');
-//var fs = require('fs');
-//var url = require('url');
-//var server = http.createServer();
-//var file = fs.createReadStream('CFIndex.html');
-//
-//var errorHandler = function(error, content) {
-//    console.error('on error: ' + error);
-//        }; 
-//server.on('error', errorHandler);
-//file.on('error', errorHandler);
-//
-//server.on('request', function(request,response) {
-//    var rpathname = url.parse(request.url).path;
-//    console.log("Request for " + rpathname + " received.");
-//    if (rpathname === '/') {
-//        console.log('There comes a correct request...');
-//        response.writeHead(200, {'content-type':'text/html'});
-//        file
-//            .pipe(response, {end:false})
-//            .on('error', errorHandler);
-//        file.on ('close', function () {
-//            response.end(console.log('server is closing'));
-//        });
-//    } else {
-//        response.writeHead(404, {'content-type':'text/plain'});
-//        response.write('Incorrect url');
-//        response.end();
-//    }
-//});
-//
-////test code
-//server.listen(8080, '127.0.0.1');
-//console.log('server is starting.');
-//server.emit('error', 'Ohno', 'xyz');
-//file.emit('error', 'again!', 'abc');
+
